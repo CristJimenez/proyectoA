8d108f6614614eb0eaf7acc5353133875a7b784d
Cristian Jimenez <123cristianjose@gmail.com>
2023-08-27 15:25:30 -0500

    proyecto

---
+++ Ventas.java
@@ -0,0 +1,9 @@
+public class Ventas {
+    public String nroRecibo;
+    public String fecha;
+    public double precio;
+    public String cliente;
+    public int num;
+    public String[] productos = new String[5];
+    public int ndp;
+}
